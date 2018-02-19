76c6417c6c9aa214884b4ccf5313d6c62c709e73
Charles Chandler <charles.chandler@willowtreeapps.com>
2018-02-19 09:53:00 -0500

    Initial Commit

---
+++ DynamicHorizontalScrollingShelves/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynamicHorizontalScrollingShelves
+//
+//  Created by Charles Chandler on 2/19/18.
+//  Copyright © 2018 Charles Chandler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
